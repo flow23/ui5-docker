b084ea3d946418f5a7985096154dd34ba966e642
Florian Wallburg <florian.wallburg@web.de>
2019-07-03 20:20:04 +0200

    Connection issues

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,16 +2,18 @@
 
 set -euo pipefail
 
-cd /usr/src/app
+cd $APP_DIR
 # RUN npm init --yes
-ui5 init
+if [ ! -f "$APP_DIR/ui5.yaml" ]; then
+  ui5 init
+fi
 
 # Install node.js project dependencies from package.json
 npm install
 
 # ???
 npm install grunt-string-replace
-npm install @openui5/sap.ui.core @openui5/themelib_sap_belize 
+npm install @openui5/sap.ui.core @openui5/themelib_sap_belize
 
 #CMD [ "ui5" , "serve", "--accept-remote-connections" ]
 exec ui5 serve --accept-remote-connections
