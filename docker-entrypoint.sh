411fd878c264ea659054f30bebaca60c3be8b141
Florian Wallburg <florian.wallburg@web.de>
2019-07-03 21:11:37 +0200

    EOW

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,18 +2,27 @@
 
 set -euo pipefail
 
-cd $APP_DIR
+# UI5 tooling check
+echo "0 # UI5 project #"
+cd "${APP_DIR}"
 # RUN npm init --yes
-if [ ! -f "$APP_DIR/ui5.yaml" ]; then
+if [ ! -f "${APP_DIR}/ui5.yaml" ]; then
+  echo "0 ## Initializing ui5 project ##"
   ui5 init
 fi
 
 # Install node.js project dependencies from package.json
+echo "1 # NPM #"
+echo "1 ## Install ##"
 npm install
+echo "1 ## Audit fix ##"
+npm audit fix
 
-# ???
+# UI5 specific stuff
+echo "2 # SAP #"
 npm install grunt-string-replace
 npm install @openui5/sap.ui.core @openui5/themelib_sap_belize
 
-#CMD [ "ui5" , "serve", "--accept-remote-connections" ]
+# Start node server
+echo "3 # Serve the app #"
 exec ui5 serve --accept-remote-connections
