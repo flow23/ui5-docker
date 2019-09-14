7eda28b97181238c068ce181ee6d186fcf5826c4
Florian Wallburg <florianwallburg@mac-004.conceto.local>
2019-09-14 19:34:00 +0200

    Commit for fit

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -25,19 +25,18 @@ npm audit fix
 
 # UI5 specific stuff
 echo "2 # SAP #"
-npm install --global grunt-cli
-npm install grunt --save-dev
-npm install grunt-string-replace --save-dev
 npm install @openui5/sap.ui.core @openui5/themelib_sap_belize
 echo "2 ## Audit fix ##"
 npm audit fix
 
 # Symlinking and downloading
 echo "3 # Symlinking #"
-mkdir -p resources test-resources/sap/ushell/bootstrap
-ln -s /usr/src/app/node_modules/@openui5/sap.ui.core/src/sap-ui-core.js resources/sap-ui-core.js
-wget -O /usr/src/util/sandbox.js "https://sapui5.hana.ondemand.com/test-resources/sap/ushell/bootstrap/sandbox.js"
-ln -s /usr/src/util/sandbox.js test-resources/sap/ushell/bootstrap/sandbox.js
+#mkdir -p webapp/resources/sap/ushell/services
+#ln -s /usr/src/app/node_modules/@openui5/sap.ui.core/src/sap-ui-core.js webapp/resources/sap-ui-core.js
+#wget -O webapp/resources/sandbox.js "https://sapui5.hana.ondemand.com/test-resources/sap/ushell/bootstrap/sandbox.js"
+#wget -O webapp/resources/sap/ushell/services/Container.js "https://sapui5.hana.ondemand.com/resources/sap/ushell/services/Container.js"
+#wget -O webapp/resources/sap/ushell/utils.js "https://sapui5.hana.ondemand.com/resources/sap/ushell/utils.js"
+#wget -O webapp/resources/sap/ushell/utils/clone.js "https://sapui5.hana.ondemand.com/resources/sap/ushell/utils/clone.js"
 
 # Start node server
 echo "4 # Serve the app #"
